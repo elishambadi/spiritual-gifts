682e972e6693588b81f26521a7bb67b480768922
Elisha Mbadi <elisha.mbadi@valuechainfactory.com>
2026-01-30 21:57:24 +0300

    fix host

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'https://spiritualgifts.elimbadi.com/api'
+const API_BASE_URL = 'https://spiritualgifts.parklandssda.org/api'
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
