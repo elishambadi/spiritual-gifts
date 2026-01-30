a460ce8244aa28cc552a5a87bbcaeed11a5d9acc
Elisha Mbadi <elisha.mbadi@valuechainfactory.com>
2026-01-30 22:21:16 +0300

    add allowed hosts

--- backend/spiritual_gifts/settings.py
+++ backend/spiritual_gifts/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-change-this-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'backend', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'backend', '0.0.0.0', 'spiritualgifts.parklandssda.org']
 
 
 # Application definition
